73cd3c3813db2e90491875b3ba14078dab77904b
Lương Thị Thương Thương <32722673+ThuongThuongtlu@users.noreply.github.com>
2018-10-24 13:46:40 +0700

    Block

---
+++ QuanLySinhVien/BlockchainWithMultipleTransactions/Interfaces/IBlock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlockChainCourse.BlockWithMultipleTransactions.Interfaces
+{
+    public interface IBlock
+    {
+        // List of transactions
+        List<ITransaction> Transaction { get; }
+
+        // Block header data
+        int BlockNumber { get; }
+        DateTime CreatedDate { get; set; }
+        string BlockHash { get; }
+        string PreviousBlockHash { get; set; }
+        void Add(int a,
+                            DateTime b,
+                            string c,
+                            string d);
+        void AddTransaction(ITransaction transaction);
+        string CalculateBlockHash(string previousBlockHash);
+        void SetBlockHash(IBlock parent);
+        IBlock NextBlock { get; set; }
+        bool IsValidChain(string prevBlockHash, bool verbose);
+    }
+}
